576f95a8cacd228132ae83d6dcd187ef4fb50820
Motje <Mohamaadflaha2014@gmail.com>
2024-03-14 11:53:47 +0100

    Deleted

--- ReservationSystem/Visitor.cs
+++ ReservationSystem/Visitor.cs
@@ -3,20 +3,12 @@ public class Visitor
     public string Name { get; set; }
     public int Age { get; set; }
     public Guid VisitorId { get; private set; }
-    public List<Ticket> Tickets { get; set; } // Add this line
     public int RondleidingChoice { get; set; }
 
     public Visitor(string name, int rondleidingChoice)
     {
         Name = name;
         VisitorId = Guid.NewGuid();
-        Tickets = new List<Ticket>(); // Initialize the list
         RondleidingChoice = rondleidingChoice;
-    }
-
-    // Method to add tickets to the visitor
-    public void AddTicket(Ticket ticket)
-    {
-        Tickets.Add(ticket);
     }
 }
