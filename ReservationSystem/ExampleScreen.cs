003042e2096a87a51dd23dd81775fcaf7cee6d53
bluefire0009 <1032628@hr.nl>
2024-03-19 17:36:48 +0100

    Remove a bug in Screen class which made the screen be one input behind what the user clicked, also fixed a typo in ExampleScreen

--- ReservationSystem/ExampleScreen.cs
+++ ReservationSystem/ExampleScreen.cs
@@ -36,6 +36,6 @@ public static class ExampleScreen
         List<StaticConsoleObject> TextBoxs = new() { new(20, 1, TextBox1Text) };
 
         Screen currentScreen = new(TextBoxs, SelectionBoxs, 1, 2,null,null);
-        currentScreen.show();
+        currentScreen.Show();
     }
 }
