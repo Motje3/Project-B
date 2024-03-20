4ec35839b3895c29ae0c903551f593380f6ce7e9
bluefire0009 <1032628@hr.nl>
2024-03-20 10:34:55 +0100

    Make the welcome box bigger, fill the selection grid, and return the new screen

--- ReservationSystem/AllAvaibleScreens.cs
+++ ReservationSystem/AllAvaibleScreens.cs
@@ -6,24 +6,30 @@ public static class AllAvaibleScreens
         StartingScreen = _makeStartingScreen();
     }
 
-    private static Screen _makeStartingScreen()
+    private static ScreenStarting _makeStartingScreen()
     {
         List<string> WelcomeString = new()
         {
             "+---------------------------------+",
+            "|                                 |",
             "|      Welcome to the museum      |",
             "|                                 |",
             "|     Please scan your ticket     |",
             "|      to register, cancel or     |",
             "|    edit your tour reservation   |",
+            "|                                 |",
             "+---------------------------------+"
         };
         StaticConsoleObject welcomeBox = new(0, 0, WelcomeString);
+        List<StaticConsoleObject> StaticObjects = new() { welcomeBox };
 
         List<string> entryTicketField = new() { "keyboard", "1", "1", "11" };
-        SelectionConsoleObject entryTicketKeyboard = new()
-        List<SelectionConsoleObject> Row1 = new() { };
-        List<SelectionConsoleObject> Row2 = new() { };
-        List<SelectionConsoleObject> Row3 = new() { };
+        SelectionConsoleObject entryTicketKeyboard = new(12, 10, entryTicketField);
+        List<SelectionConsoleObject> Row0 = new() { null, null, null };
+        List<SelectionConsoleObject> Row1 = new() { null, entryTicketKeyboard, null };
+        List<SelectionConsoleObject> Row2 = new() { null, null, null };
+
+        List<List<SelectionConsoleObject>> SelectionGrid = new() { Row0, Row1, Row2 };
+        return new(StaticObjects,SelectionGrid,1,1,null,null);
     }
 }
