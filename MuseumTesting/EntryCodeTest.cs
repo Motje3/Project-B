5fce60002dccb44fde97f710aff0c014655b8f01
bluefire0009 <1032628@hr.nl>
2024-04-03 14:42:01 +0200

    change the string that is used in each path to a const variable which is the string

--- MuseumTesting/EntryCodeTest.cs
+++ MuseumTesting/EntryCodeTest.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 public class EntryCodeTest
 {
     private List<string> _EntryCodesAtTheStart = new() { };
+    private const string JSONPath = "../ReservationSystem/JSON-Files/sample_codes.json";
 
     [TestInitialize]
     public void Setup()
@@ -33,7 +34,7 @@ public class EntryCodeTest
     // Saves the current contents of sample_codes.json to the variable _EntryCodesAtTheStart
     private void _rememberJSON()
     {
-        using (StreamReader reader = new StreamReader("../ReservationSystem/JSON-Files/sample_codes.json"))
+        using (StreamReader reader = new StreamReader(JSONPath))
         {
             string jsonData = reader.ReadToEnd();
             _EntryCodesAtTheStart = JsonConvert.DeserializeObject<List<string>>(jsonData);
@@ -43,7 +44,7 @@ public class EntryCodeTest
     // Reverts the contents of sample_coes.json back to what it was when rememberJSON() was used
     private void _forgetJSON()
     {
-        using (StreamWriter writer = new StreamWriter("../ReservationSystem/JSON-Files/sample_codes.json"))
+        using (StreamWriter writer = new StreamWriter(JSONPath))
         {
             string List2Json = JsonConvert.SerializeObject(_EntryCodesAtTheStart);
             writer.Write(List2Json);
@@ -54,13 +55,22 @@ public class EntryCodeTest
     private void _addNewCode(int code)
     {
         _rememberJSON();
-        using (StreamWriter writer = new StreamWriter("../ReservationSystem/JSON-Files/sample_codes.json"))
+        using (StreamWriter writer = new StreamWriter(JSONPath))
         {
             List<string> toWrite = new(_EntryCodesAtTheStart);
             toWrite.Add(code.ToString());
 
             string List2Json = JsonConvert.SerializeObject(toWrite);
             writer.Write(List2Json);
+        }
+    }
+
+    private List<string> _readJSON()
+    {
+        using (StreamReader reader = new StreamReader(JSONPath))
+        {
+            string jsonData = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<List<string>>(jsonData);
         }
     }
 }
