96a4233c72566d2590879e710a1b2d80cc52e60b
bluefire0009 <1032628@hr.nl>
2024-04-12 17:13:16 +0200

    + Add comment to AddAllToursToJson

--- ReservationSystem/Program.cs
+++ ReservationSystem/Program.cs
@@ -14,9 +14,13 @@
         //GuidedTour.RemoveTourFromJSON(test);
         //var test6 = GuidedTour.ReturnAllToursFromThisYear();
         //var test3 = GuidedTour.ReturnAllCurrentToursFromToday();
-        
+        var completed = GuidedTour.CompletedTours;
+        var current = GuidedTour.CurrentTours;
+        var deleted = GuidedTour.DeletedTours;
+        var toursOfToDay = GuidedTour.ReturnAllCurrentToursFromToday();
     }
 
+    // This will take a couple of minutes do not use unless REALLY nesecasry
     public static void AddAllToursToJson()
     {
         foreach (var tour in GuidedTour.ReturnAllToursFromThisYear())
