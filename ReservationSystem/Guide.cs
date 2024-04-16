d4ee8b629043e53b63a328fe5cc70ad1d5a6b463
BoraHR <1066706@hr.nl>
2024-04-16 12:50:40 +0200

    - Modified existing Guide class to check if a visitor is in a our

--- ReservationSystem/Guide.cs
+++ ReservationSystem/Guide.cs
@@ -1,9 +1,32 @@
+// **CheckInVisitor **moet variable **GuidedTour tour = GuidedTour.FindTourById(AssingedTourId)**
+
 public class Guide : Visitor
 {
     public Guide(string name, string ticketcode) : base(name, ticketcode) {}
     public override string ToString()
     {
         return $"Name: {Name}\nTicket code: {TicketCode}";
+    }
+    public void CheckInVisitor(Visitor visitor)
+    {
+        GuidedTour tour = GuidedTour.FindTourById(base.AssingedTourId);
+
+        if (tour.ExpectedVisitors.Contains(visitor))
+        {
+            Console.WriteLine($"\nWelcome {visitor.Name} to our museum");
+            return;
+        }
+        if (tour.PresentVisitors.Contains(visitor))
+        {
+            Console.WriteLine($"\nVisitor {visitor.Name} already joine the tour");
+            return;
+        }
+        if (!tour.PresentVisitors.Contains(visitor))
+        {
+            tour.PresentVisitors.Add(visitor);
+            Console.WriteLine($"\nVisitor is not Registerd");
+            return;
+        }  
     }
 }
 
