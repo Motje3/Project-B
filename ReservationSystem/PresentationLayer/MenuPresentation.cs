3f5cfec7c17acaadb74f70ee984f5a7e975e3a3c
Motje3 <mohamaadflaha2014@gmail.com>
2024-05-08 13:19:48 +0200

    instade of exit, back to main menu in menu presentation

--- ReservationSystem/PresentationLayer/MenuPresentation.cs
+++ ReservationSystem/PresentationLayer/MenuPresentation.cs
@@ -18,7 +18,7 @@ public class MenuPresentation : View
             WriteLine("\nPlease choose an option:");
             WriteLine("1. Change my reservation time");
             WriteLine("2. Cancel my tour reservation");
-            WriteLine("3. Exit");
+            WriteLine("3. Back to Main Menu");
             Write("\nEnter your choice: ");
             string choice = ReadLine();
 
