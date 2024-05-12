df4d2425525c4ad32f0552c6e4e78199db1c2914
herkam3 <10782044@hr.nl>
2024-05-12 15:33:11 +0200

    Admin & Guide Backend testing.

---
+++ MuseumTesting/AdminBackEndTest.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MuseumTesting
+{
+    [TestClass]
+    public class AdminBackEndTests
+    {
+        [TestMethod]
+        public void ChangeTourCapacity_NotImplemented_ExceptionThrown()
+        {
+            // Act & Assert
+            Assert.ThrowsException<NotImplementedException>(() => AdminBackEnd.ChangeTourCapacity());
+        }
+
+        [TestMethod]
+        public void ChangeTourTime_NotImplemented_ExceptionThrown()
+        {
+            // Act & Assert
+            Assert.ThrowsException<NotImplementedException>(() => AdminBackEnd.ChangeTourTime());
+        }
+
+        [TestMethod]
+        public void ReadPassword_ReturnsCorrectPassword()
+        {
+            // Arrange
+            string expectedPassword = "test123";
+
+            // Simulate user input
+            Console.SetIn(new System.IO.StringReader(expectedPassword + Environment.NewLine));
+
+            // Act
+            string actualPassword = AdminBackEnd.ReadPassword();
+
+            // Assert
+            Assert.AreEqual(expectedPassword, actualPassword);
+        }
+    }
+}
