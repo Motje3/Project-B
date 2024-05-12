3f80a1b9b42ee79a98af4604fc5c71bf2e8d0c60
herkam3 <10782044@hr.nl>
2024-05-12 15:17:58 +0200

    Tour class Testing and adjustments

--- MuseumTesting/TicketsTest.cs
+++ MuseumTesting/TicketsTest.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 
-namespace MuseumTesting.Tests
+namespace MuseumTesting
 {
     [TestClass]
     public class TicketTests
