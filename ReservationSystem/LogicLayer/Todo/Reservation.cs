6b897cbb6b9b5a9c4a9f370eb576500693ae4c9e
BoraHR <1066706@hr.nl>
2024-05-13 19:06:37 +0200

    - Moved remaining Logic classes to todo (excluding done)

--- ReservationSystem/LogicLayer/Todo/Reservation.cs
+++ ReservationSystem/LogicLayer/Todo/Reservation.cs
@@ -47,4 +47,29 @@ public class ReservationPresentation : View
         }
     }
 
+    public static void ShowRestrictedMenu(Visitor visitor)
+    {
+        MenuLogic.JoinTour(visitor);
+    }
+
+    public static void ShowFullMenu(Visitor visitor)
+    {
+        MenuLogic logic = new MenuLogic();
+        bool choosingOption = true;
+        while (choosingOption)
+        {
+            string reservationDetails = Visitor.GetCurrentReservation(visitor);
+            Console.WriteLine(reservationDetails);
+            Console.WriteLine("\nPlease choose an option:");
+            Console.WriteLine("1. Change my reservation time");
+            Console.WriteLine("2. Cancel my tour reservation");
+            Console.WriteLine("3. Return to main menu");
+            Console.Write("\nEnter your choice: ");
+            string choice = Console.ReadLine();
+
+            choosingOption = logic.HandleFullMenuChoice(choice, visitor);
+
+        }
+    }
+
 }
