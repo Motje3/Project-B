4fba5ba0f6c4da8bb18df12b7fdc54f73b3c176b
Motje3 <mohamaadflaha2014@gmail.com>
2024-05-13 12:26:34 +0200

    dd

--- ReservationSystem/LogicLayer/Guide.cs
+++ ReservationSystem/LogicLayer/Guide.cs
@@ -105,7 +105,9 @@ public class Guide
         // Save changes
         Tour.SaveTours();
 
-        // Confirmation message
+        try { Console.Clear(); } catch { }
         Console.WriteLine($"Guide {Guide.AllGuides[guideIndex].Name} has been successfully assigned to the tour at {Tour.TodaysTours[tourIndex].StartTime:hh:mm tt} o'clock.");
+        Thread.Sleep(2000);
+        try { Console.Clear(); } catch { }
     }
 }
