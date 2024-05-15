300327d28d7b4f325ec02f1bfb562c78f387ab9b
bluefire0009 <1032628@hr.nl>
2024-05-15 14:17:22 +0200

    + Add ReadKey for FakeWorld so we can test password of the admin

--- MuseumTesting/FakeWorld.cs
+++ MuseumTesting/FakeWorld.cs
@@ -1,4 +1,5 @@
-﻿namespace ReservationSystem;
+﻿
+namespace ReservationSystem;
 
 public class FakeWorld : IWorld
 {
@@ -21,7 +22,7 @@ public class FakeWorld : IWorld
     {
         LinesWritten.Add(line);  // keep in mind it stores it like WritLine as single string in list
     }
-    
+
     public List<string> LinesToRead { private get; set; } = new();
 
     public string ReadLine()
@@ -41,5 +42,32 @@ public class FakeWorld : IWorld
     public void WriteAllText(string path, string contents)
     {
         Files[path] = contents;
+    }
+
+    public ConsoleKeyInfo ReadKey(bool intercept)
+    {
+
+        if (!LinesToRead.Last().Contains(","))
+        {
+            throw new FormatException("Input is not comma seperated");
+        }
+
+        String[] unEdditedInput = LinesToRead[0].Trim(',').Split(",");
+        string nextInput = unEdditedInput[0];
+        unEdditedInput[0] = null;
+
+
+        LinesToRead[0] = String.Join(",", unEdditedInput);
+
+        if (LinesToRead[0] == "")
+            LinesToRead.RemoveAt(0);
+
+        ConsoleKey pressed;
+        if (nextInput.Length == 1)
+            pressed = (ConsoleKey)Enum.Parse(typeof(ConsoleKey), nextInput.ToUpper());
+        else
+            pressed = (ConsoleKey)Enum.Parse(typeof(ConsoleKey), nextInput);
+        ConsoleKeyInfo info = new('0', pressed, false, false, false);
+        return info;
     }
 }
