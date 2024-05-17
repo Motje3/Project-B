c8134f2810dd94f40880f9f82987a188aea149ad
Motje <Mohamaadflaha2014@gmail.com>
2024-05-17 12:28:25 +0200

    added intilizing tours to main

--- ReservationSystem/LogicLayer/Program.cs
+++ ReservationSystem/LogicLayer/Program.cs
@@ -5,6 +5,8 @@ public class Program
     public static IWorld World = new RealWorld();
     public static void Main()
     {
+        Guide.LoadGuides();
+        Tour.InitializeTours();
         ReservationPresentation.ValidateCodeAndShowMenu();
     }
 }
