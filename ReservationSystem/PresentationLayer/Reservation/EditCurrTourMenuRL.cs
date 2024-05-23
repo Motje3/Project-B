f111fa0642add16e209855c5f0a091c97aed1c6e
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2024-05-23 12:40:47 +0200

    * Replace some Console.WriteLine

--- ReservationSystem/PresentationLayer/Reservation/EditCurrTourMenuRL.cs
+++ ReservationSystem/PresentationLayer/Reservation/EditCurrTourMenuRL.cs
@@ -5,9 +5,9 @@ public class EditCurrTourMenuRL : View
     public static string Show(string reservationDetails)
     {
         WriteLine(reservationDetails);
-        Console.Write("\nPlease choose an option by entering the ");
+        Write("\nPlease choose an option by entering the ");
         ColourText.WriteColored("", "number ", ConsoleColor.Cyan, "left of the desired action\n");
-        Program.World.WriteLine("");
+        WriteLine("");
 
 
         // Save the current console color
@@ -15,19 +15,19 @@ public class EditCurrTourMenuRL : View
 
         // Change the color to cyan for the numbers
         Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.Write("1 | ");
+        Write("1 | ");
         Console.ForegroundColor = originalColor; // Reset to the original color
-        Console.WriteLine("Change my reservation time");
+        WriteLine("Change my reservation time");
 
         Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.Write("2 | ");
+        Write("2 | ");
         Console.ForegroundColor = originalColor; // Reset to the original color
-        Console.WriteLine("Cancel my tour reservation");
+        WriteLine("Cancel my tour reservation");
 
         Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.Write("3 | ");
+        Write("3 | ");
         Console.ForegroundColor = originalColor; // Reset to the original color
-        Console.WriteLine("Return to Main Menu");
+        WriteLine("Return to Main Menu");
 
         Write("\nEnter your choice: ");
         return ReadLine();  // choice
