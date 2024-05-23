6fee8c3d9afff2f217625457a58db0161c2ef41e
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2024-05-23 11:51:12 +0200

    + Add system test to check amount of printed tours

--- MuseumTesting/SysTestTourJoinen.cs
+++ MuseumTesting/SysTestTourJoinen.cs
@@ -91,7 +91,7 @@ namespace MuseumTesting
         public void TestVisitorJoinTourMenuFailedOptionDoesNotExit()
         {
             // Arrange
-            string tourChoice = "1000";
+            string tourChoice = "XXX";
             string visitorTicketCode = "1234567890";
             FakeWorld world = new()
             {
@@ -115,6 +115,45 @@ namespace MuseumTesting
 
             // Assert
             Assert.IsTrue(((FakeWorld)Program.World).LinesWritten.Contains("*    Invalid choice    *"));
+        }
+
+        [TestMethod]
+        public void TestAvaliableToursShow()
+        {
+            // Arrange
+            int linesPrintedPerTour = 6;
+            string visitorTicketCode = "1234567890";
+            FakeWorld world = new()
+            {
+                Today = new DateTime(2024, 6, 1),
+                Now = new DateTime(2024, 6, 1, 12, 0, 0),
+                Files =
+                {
+                    {"./JSON-Files/OnlineTickets.json", "[\"1234567890\"]"},
+                    {"./JSON-Files/TourSettings.json","{\"StartTime\": \"11:00:00\",\"EndTime\": \"16:15:00\",\"Duration\": 15,\"MaxCapacity\": 13}"},
+                    {"./JSON-Files/GuideAssignments.json","[{\"GuideName\": \"John Doe\",\"Tours\": [{\"StartTime\": \"11:00 AM\"},{\"StartTime\": \"12:15 PM\"},{\"StartTime\": \"01:30 PM\"},{\"StartTime\": \"02:45 PM\"},{\"StartTime\": \"04:00 PM\"}]},{\"GuideName\": \"Alice Johnson\",\"Tours\": [{\"StartTime\": \"11:15 AM\"},{\"StartTime\": \"12:30 PM\"},{\"StartTime\": \"01:45 PM\"},{\"StartTime\": \"03:00 PM\"},{\"StartTime\": \"04:15 PM\"}]},{\"GuideName\": \"Steve Brown\",\"Tours\": [{\"StartTime\": \"11:30 AM\"},{\"StartTime\": \"12:45 PM\"},{\"StartTime\": \"02:00 PM\"},{\"StartTime\": \"03:15 PM\"}]},{\"GuideName\": \"Mary Lee\",\"Tours\": [{\"StartTime\": \"11:45 AM\"},{\"StartTime\": \"01:00 PM\"},{\"StartTime\": \"02:15 PM\"},{\"StartTime\": \"03:30 PM\"}]},{\"GuideName\": \"Tom Clark\",\"Tours\": [{\"StartTime\": \"12:00 PM\"},{\"StartTime\": \"01:15 PM\"},{\"StartTime\": \"02:30 PM\"},{\"StartTime\": \"03:45 PM\"}]}]"}
+                },
+                LinesToRead = new()
+                {
+                    visitorTicketCode,"GETMEOUT","GETMEOUT"
+                }
+            };
+            Program.World = world;
+
+            // Act
+            Program.Main();
+
+            // Assert
+            List<Tour> availableTours = Tour.TodaysTours
+                .Where(tour => !tour.Completed && !tour.Deleted && tour.ExpectedVisitors.Count < tour.MaxCapacity && tour.StartTime > Program.World.Now)
+                .OrderBy(tour => tour.StartTime)
+                .ToList();
+            
+            int amountToursRightNow = availableTours.Count;
+            int expectedToursPrinted = amountToursRightNow * linesPrintedPerTour;
+
+            // If each tour printed is {linesPrintedPerTour} lines then LinesWritten should contain more than {linesPrintedPerTour} lines.
+            Assert.IsTrue(((FakeWorld)Program.World).LinesWritten.Count > expectedToursPrinted);
         }
     }
 }
