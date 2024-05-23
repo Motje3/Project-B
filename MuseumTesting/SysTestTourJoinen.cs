109b8a6ee147eee9493ef9dc66536a357efbb23c
bluefire0009 <1032628@hr.nl>
2024-05-23 09:09:10 +0200

    + Start writing second system test

--- MuseumTesting/SysTestTourJoinen.cs
+++ MuseumTesting/SysTestTourJoinen.cs
@@ -7,10 +7,10 @@ namespace MuseumTesting
     public class SysTestTourJoinen
     {
         [TestMethod]
+
         public void TestVisitorJoinTourMenuSucceful()
         {
             // Arrange
-            string pathToursToday = "./JSON-Files/Tours-20240601.json";
             string tourChoice = "1";
             string visitorTicketCode = "1234567890";
             FakeWorld world = new()
@@ -44,6 +44,35 @@ namespace MuseumTesting
             Assert.IsTrue(Visitor.FindVisitorByTicketCode(visitorTicketCode) != null);
             File.Delete(Tour.JsonFilePath);
             // clean up for this specific test method
+        }
+
+        [TestMethod]
+        public void TestVisitorJoinTourMenuFailedNoToursAvaliable()
+        {
+            // Arrange
+            string visitorTicketCode = "1234567890";
+            FakeWorld world = new()
+            {
+                Today = new DateTime(2024, 6, 1),
+                Now = new DateTime(2024, 6, 1, 18, 0, 0),
+                Files =
+                {
+                    {"./JSON-Files/OnlineTickets.json", "[\"1234567890\"]"},
+                    {"./JSON-Files/TourSettings.json","{\"StartTime\": \"11:00:00\",\"EndTime\": \"16:15:00\",\"Duration\": 15,\"MaxCapacity\": 13}"},
+                    {"./JSON-Files/GuideAssignments.json","[{\"GuideName\": \"John Doe\",\"Tours\": [{\"StartTime\": \"11:00 AM\"},{\"StartTime\": \"12:15 PM\"},{\"StartTime\": \"01:30 PM\"},{\"StartTime\": \"02:45 PM\"},{\"StartTime\": \"04:00 PM\"}]},{\"GuideName\": \"Alice Johnson\",\"Tours\": [{\"StartTime\": \"11:15 AM\"},{\"StartTime\": \"12:30 PM\"},{\"StartTime\": \"01:45 PM\"},{\"StartTime\": \"03:00 PM\"},{\"StartTime\": \"04:15 PM\"}]},{\"GuideName\": \"Steve Brown\",\"Tours\": [{\"StartTime\": \"11:30 AM\"},{\"StartTime\": \"12:45 PM\"},{\"StartTime\": \"02:00 PM\"},{\"StartTime\": \"03:15 PM\"}]},{\"GuideName\": \"Mary Lee\",\"Tours\": [{\"StartTime\": \"11:45 AM\"},{\"StartTime\": \"01:00 PM\"},{\"StartTime\": \"02:15 PM\"},{\"StartTime\": \"03:30 PM\"}]},{\"GuideName\": \"Tom Clark\",\"Tours\": [{\"StartTime\": \"12:00 PM\"},{\"StartTime\": \"01:15 PM\"},{\"StartTime\": \"02:30 PM\"},{\"StartTime\": \"03:45 PM\"}]}]"}
+                },
+                LinesToRead = new()
+                {
+                    visitorTicketCode,"GETMEOUT"
+                }
+            };
+            Program.World = world;
+
+            // Act
+            Program.Main();
+
+            // Assert
+            Assert.IsTrue();
         }
     }
 }
