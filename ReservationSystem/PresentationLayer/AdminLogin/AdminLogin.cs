3a84c11a9856a24fded71850dabd7aa6a19db02d
Motje <Mohamaadflaha2014@gmail.com>
2024-05-28 19:53:21 +0200

    done

--- ReservationSystem/PresentationLayer/AdminLogin/AdminLogin.cs
+++ ReservationSystem/PresentationLayer/AdminLogin/AdminLogin.cs
@@ -5,13 +5,15 @@ public class AdminLogin : View
 {
     public static void Show()
     {
+        try { Console.Clear(); } catch { }
+
         Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine();
         Console.WriteLine(" -------------------------");
         Console.WriteLine("|       Admin login       |");
         Console.WriteLine(" -------------------------");
         Console.ResetColor();
-        Console.WriteLine(); // Ensure the cursor moves to the next line after the box
+        Console.WriteLine("Password: "); // Ensure the cursor moves to the next line after the box
 
     }
 }
