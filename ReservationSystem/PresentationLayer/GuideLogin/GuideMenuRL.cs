22c2a7824823e4e0a3291d89920c32e083a9c546
Motje3 <mohamaadflaha2014@gmail.com>
2024-05-29 09:31:46 +0200

    fix guide menu to have its own lines

--- ReservationSystem/PresentationLayer/GuideLogin/GuideMenuRL.cs
+++ ReservationSystem/PresentationLayer/GuideLogin/GuideMenuRL.cs
@@ -6,9 +6,9 @@ public class GuideMenuRL : View
     {
         WriteLine("\nGuide Menu:\n");
         ColourText.WriteColored("", "1 |", ConsoleColor.Cyan, " View personal Tours");
-        ColourText.WriteColored("", "2 |", ConsoleColor.Cyan, " Start Upcoming Tour");
-        ColourText.WriteColored("", "3 |", ConsoleColor.Cyan, " Add visitor for upcoming tour");
-        ColourText.WriteColored("", "4 |", ConsoleColor.Cyan, " Back to visitor's Menu");
+        ColourText.WriteColored("\n", "2 |", ConsoleColor.Cyan, " Start Upcoming Tour");
+        ColourText.WriteColored("\n", "3 |", ConsoleColor.Cyan, " Add visitor for upcoming tour");
+        ColourText.WriteColored("\n", "4 |", ConsoleColor.Cyan, " Back to visitor's Menu\n");
 
         Write("\nEnter your choice: ");
         return ReadLine();
