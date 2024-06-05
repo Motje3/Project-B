c7edf7772e8a63a763ce52d57732d492d3555bd4
bluefire0009 <1032628@hr.nl>
2024-06-05 11:31:48 +0200

    * Replace some old File.ReadAllText

--- ReservationSystem/DataAccessLayer/JsonHelper.cs
+++ ReservationSystem/DataAccessLayer/JsonHelper.cs
@@ -1,17 +1,18 @@
 using Newtonsoft.Json;
+using ReservationSystem;
 
 public static class JsonHelper
 {
     public static T LoadFromJson<T>(string filePath)
     {
-        string json = File.ReadAllText(filePath);
+        string json = Program.World.ReadAllText(filePath);
         return JsonConvert.DeserializeObject<T>(json);
     }
 
     public static void SaveToJson<T>(T objectToSave, string filePath)
     {
         string json = JsonConvert.SerializeObject(objectToSave, Formatting.Indented);
-        File.WriteAllText(filePath, json);
+        Program.World.WriteAllText(filePath, json);
     }
 
     // Specialized method to handle list operations like add, delete, and update for GuidedTour
